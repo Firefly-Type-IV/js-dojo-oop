5ba2236eb4590a70ac02c34594a7a80ed7682acd
Sinth Uthayakumar <sinthuk10@gmail.com>
2025-10-11 21:15:08 +0100

    17 .indexOf() array method.

--- loops/17.js
+++ loops/17.js
@@ -14,4 +14,5 @@ export function findIndex(arr, element) {
   // TODO: For each element, check if it is equal to the specified element.
   // TODO: If it is, return the current index.
   // TODO: If the loop completes without finding the element, return -1.
+  return arr.indexOf(element);
 }

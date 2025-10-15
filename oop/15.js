d9e764bb1de5245ab66810952d0104b4b26807bb
Sinth Uthayakumar <sinthuk10@gmail.com>
2025-10-15 17:01:42 +0100

    15 Inheritance and method overriding.

--- oop/15.js
+++ oop/15.js
@@ -8,6 +8,7 @@ export class Shape {
    */
   getArea() {
     // TODO: Return 0 as a default implementation.
+    return 0
   }
 }
 
@@ -22,6 +23,10 @@ export class Rectangle extends Shape {
   constructor(width, height) {
     // TODO: Call super() to invoke the parent constructor.
     // TODO: Store width and height as properties.
+    super(width, height)
+    // The super() method calls the parent class's constructor, passing in any arguments that are needed for the parent class's constructor.
+    this.width = width;
+    this.height = height;
   }
 
   /**
@@ -30,5 +35,9 @@ export class Rectangle extends Shape {
    */
   getArea() {
     // TODO: Return width * height.
+    return this.width * this.height;
   }
+  // Use inheritance to promote an 'is a' relationship between objects and promote code reuse and common behaviour i.e. savings account 'is a' type of account. For every piece of shared functionality a new method doesn't have to be created
+  // polymorphism 'many forms'allows objects of different classes to be treated as common type.. elaborate
+  // method overriding - powerful, sub class provides a specific implementation for a method that is already defined in its super class. In previous example dog reused makeSound() method but returned barks.
 }

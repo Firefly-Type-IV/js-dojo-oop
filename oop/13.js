de93e50704b5f69a4e0d740e00e44f5a2cb03784
Sinth Uthayakumar <sinthuk10@gmail.com>
2025-10-15 16:38:27 +0100

    13 Use pythagorean theorem to calculate distance

--- oop/13.js
+++ oop/13.js
@@ -8,6 +8,8 @@ export class Point {
    */
   constructor(x, y) {
     // TODO: Store x and y as properties.
+    this.x = x;
+    this.y = y; 
   }
 
   /**
@@ -23,5 +25,19 @@ export class Point {
   distanceTo(other) {
     // TODO: Calculate distance using the formula: sqrt((x2-x1)² + (y2-y1)²)
     // TODO: Use Math.sqrt and Math.pow
+    this.x
+    this.y
+    other.x // another instance of the Point parent class
+    other.y
+    // subtraction, squaring (Math.pow), addition, square root (Math.sqrt)
+    // Pythagorean theorem a squared plus b squared equals c squared, we want hypotenuse
+    let deltaX = other.x - this.x
+    let deltaY = other.y - this.y
+    // using the legs of triangle, apply math.pow to square difference in x and y by 2
+    const distanceSquared = Math.pow(deltaX, 2) + Math.pow(deltaY, 2)
+    // output c/hypotenuse/distance
+    const distance =  Math.sqrt(distanceSquared)
+    return distance
+    // variables and functions are camelCase, classes are PascalCase, constants are SCREAMING_SNAKE_CASE
   }
 }
